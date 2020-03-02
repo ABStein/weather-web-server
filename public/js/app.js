39f776ec12e92f70e0834139add129033f859ab5
ABStein <abs19stein@gmail.com>
2020-03-02 17:22:03 -0600

    feat: fetched weather on the client and render, added slight style

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,29 @@
-fetch('http://localhost:3000/weather?address=boston').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            console.log(data.error);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
-    }); 
-});
+const weatherFrom = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+messageOne.textContent = ''
+messageTwo.textContent = ''
+
+
+weatherFrom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value
+    
+    //  placeholder/making sure the second line is cleared out.
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+    
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+                // messageTwo.textContent = ''
+            } else {
+                messageOne.textContent = `Location: ${data.location}`
+                messageTwo.textContent = `Forecast: ${data.forecast}`
+            }
+        })
+    })
+})
