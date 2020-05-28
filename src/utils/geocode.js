608b57d176bfba77e9a14860563cf51b49e7c853
ABStein <abs19stein@gmail.com>
2020-05-27 22:01:03 -0500

    fix: trying again 2

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -4,11 +4,11 @@ const dotenv = require('dotenv').config('.env');
 
 const geocode =  (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.MAPBOX_API_KEY}`;
-
+    console.log('geocode url:' , url)
     request({ url, json: true }, (error, { body }) => {
         if (error) {
              callback('There was an issue connecting to the server. Please try again later.')
-        } else if (body.features.length === 0 || body === undefined) {
+        } else if (body === undefined || body.features.length === 0) {
              callback('Unable to find location. Try another search!')
         } else {
              callback(undefined, {
